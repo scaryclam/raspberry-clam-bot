f66a2a6731ae2f4c1770ff58d24e1ff1e2dac28b
Becky Lewis <becky.lewis@tangentlabs.co.uk>
2012-07-19 17:00:39 +0100

    Added config

---
+++ tweeter.py
@@ -0,0 +1,6 @@
+from twitterbot.twit import TwitterBot
+
+if __name__ == '__main__':
+    tweeter = TwitterBot(config_file="secrets.conf")
+    #tweeter.send_tweet("Hello World!")
+
--- twitterbot/twit.py
+++ twitterbot/twit.py
@@ -1,11 +1,21 @@
 import tweepy
+import ConfigParser
+
 
 class TwitterBot(object):
-    def __init__(self):
-        self.CONSUMER_KEY = 'shhh, its a secret!'
-        self.CONSUMER_SECRET = 'shhh, its a secret!'
-        self.ACCESS_TOKEN = 'shhh, its a secret!'
-        self.ACCESS_SECRET = 'shhh, its a secret!'
+    def __init__(self, config_file=None):
+        if config_file:
+            config = ConfigParser.ConfigParser()
+            config.readfp(open(config_file))
+            self.CONSUMER_KEY = config.get("API", "consumer_key")
+            self.CONSUMER_SECRET = config.get("API", "consumer_secret")
+            self.ACCESS_TOKEN = config.get("API", "access_token")
+            self.ACCESS_SECRET = config.get("API", "access_secret")
+        else:
+            self.CONSUMER_KEY = 'shhh, its a secret!'
+            self.CONSUMER_SECRET = 'shhh, its a secret!'
+            self.ACCESS_TOKEN = 'shhh, its a secret!'
+            self.ACCESS_SECRET = 'shhh, its a secret!'
 
         auth = tweepy.OAuthHandler(self.CONSUMER_KEY, self.CONSUMER_SECRET)
         auth.set_access_token(self.ACCESS_TOKEN, self.ACCESS_SECRET)
